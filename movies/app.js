899e40937e572197d17d801d0df2179896dd46eb
Zaeem Ali <68733116+zaeemali272@users.noreply.github.com>
2023-07-15 15:22:39 +0500

    movies

---
+++ movies/app.js
@@ -0,0 +1,44 @@
+
+//TOGGLE
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,.caret,.dropdown,.menu,.endding-line,a:visited,.menu-list,.navbar-title,.navbar,.movie-container,.navbar-title-now,.menu li,.movie-list-item-img,.featured-content"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
+
+//DROPDOWN
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li')
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click',() => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    }); 
+    option.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option => {
+                option.classList.remove('active')
+            });
+            option.classList.add('active');
+        });
+    });
+});
